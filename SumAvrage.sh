c97206003427b7b87eb1c63b1e34616b32fea6f0
kumudinidesale <kdesale1211@gmail.com>
2022-01-14 21:29:42 +0530

    SA

---
+++ SumAvrage.sh
@@ -0,0 +1,15 @@
+
+
+
+#!/bin/bash -x
+
+var1=$1
+var2=$2
+var3=$3
+var4=$4
+var5=$5
+
+sum=$(( $var1 + $var2 + $var3 + $var4 + $var5 ))
+echo "Sum=$sum"
+average=$(( $sum / 5 ))
+echo "Average=$average"

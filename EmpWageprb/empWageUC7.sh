5325eb2d6d694d1efe24f3c989439189805ae48b
kumudinidesale <kdesale1211@gmail.com>
2022-01-18 21:36:24 +0530

    salerymonth

---
+++ EmpWageprb/empWageUC7.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+isFullTime=1;
+isPartTime=2;
+totalSalary=0;
+empRatePerHrs=20;
+maxWorkingDays=20;
+maxWorkingHrs=100;
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+function getWorkingHrs(){
+	case $1 in
+      $isFullTime) 
+         empHrs=8
+         ;;
+      $isPartTime)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+	echo $empHrs
+}
+
+function getEmpWagePerDay(){
+	echo $(($1*$empRatePerHrs))
+}
+while [[ $totalEmpHrs -lt $maxWorkingHrs && $totalWorkingDays -le $maxWorkingDays ]]
+do
+((totalWorkingDays++))
+	empCheck=$((RANDOM%3));
+	empHrs=$( getWorkingHrs $empCheck )
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	dailyWage[$totalWorkingDays]=$( getEmpWagePerDay $empHrs)
+done
+
+totalSalary=$(($totalEmpHrs * $empRatePerHrs))
+echo $totalSalary
+echo ${dailyWage[@]}
